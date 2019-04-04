c909aa3be052439987a8f97d404f119426fa2c94
Adão Lima <adaoolima@gmail.com>
2019-04-04 00:31:56 -0300

    Setting the structure of project

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,65 @@
 
-const { src, dest, parallel, watch } = require('gulp');
+const { src, dest, parallel, watch} = require('gulp');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
-const autoprefixer = require('autoprefixer');
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+const reload =  browserSync.reload;
+
+var ENV = "DEV";
+var condition = ENV == "DEV" ? { sourcemaps: true } : { sourcemaps: false };
 
 sass.compiler = require('node-sass');
 
 
     function html() {
-        return src('src/templates/*.pug')
+        return src(['src/views/*.pug','src/views/_04-pages/*.pug'])
             .pipe(pug())
-            .pipe(dest('dist/html'));
-        
+            .pipe(dest('app'))
+            .pipe(reload({stream: true}))
     }
 
-    function css() {       
-        return src('src/styles/*.scss')
+    function css() {
+        return src('src/styles/*.scss', condition)
             .pipe(sass())
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
             .pipe(minifyCSS())
-            .pipe(dest('dist/css'));
-       
+            .pipe(concat('app.min.css'))
+            .pipe(dest('app/css', condition ))
+            .pipe(reload({stream: true}))
     }
 
     function js() {
-        return src('src/scripts/*.js', { sourcemaps: true })
+        return src('src/scripts/*.js', condition)
+            .pipe(babel())
             .pipe(concat('app.min.js'))
-            .pipe(dest('dist/js', { sourcemaps: true }));
+            .pipe(dest('app/js', condition))
+            .pipe(reload({stream: true}))
     }
+
+    function watcher() {
+        browserSync({server: './app'});
+        watch('src/views/*.pug', html);
+        watch('src/styles/*.scss', css);
+        watch('src/scripts/*.js', js);
+    }
+
+    function purifyIt() {
+
+    }
+
   
+  exports.watcher = watcher;
   exports.js = js;
   exports.css = css;
   exports.html = html;
+
+ 
 
   exports.default = parallel(html, css, js);
