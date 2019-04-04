fafc4e734453f42aa244e2b5695b927165f7a6ce
Adão Lima <adaoolima@gmail.com>
2019-04-04 12:12:04 -0300

    Added bootstrap lib

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,11 @@ const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const autoprefixer = require('gulp-autoprefixer');
+const inject = require('gulp-inject'); // Add the inject task
 const browserSync = require('browser-sync');
 const reload =  browserSync.reload;
 
-var ENV = "DEV";
+var ENV = "PROD";
 var condition = ENV == "DEV" ? { sourcemaps: true } : { sourcemaps: false };
 
 sass.compiler = require('node-sass');
@@ -50,16 +51,26 @@ sass.compiler = require('node-sass');
         watch('src/scripts/*.js', js);
     }
 
-    function purifyIt() {
-
+    function getAssets() {
+        return src('assets/*')
+            .pipe(dest('app/assets'))
     }
 
+    function build(cb) {
+        ENV = "PROD";
+        html(); 
+        css();
+        js();
+        cb();
+    }
   
   exports.watcher = watcher;
   exports.js = js;
   exports.css = css;
   exports.html = html;
+  exports.getAssets = getAssets;
 
  
 
   exports.default = parallel(html, css, js);
+  exports.build = build;
