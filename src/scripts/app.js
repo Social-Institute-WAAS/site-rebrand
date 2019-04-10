4bc263df986cf87ba89fec0971eeb9ab73790f41
Adao Lima <adaoolima@gmail.com>
2019-04-10 18:16:12 -0300

    Updated html structure of home

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,15 @@
 
-  console.log('JAVASCRIPT LOADED!');
+// FIXED NAVBAR
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop + 0.5;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("fixed-top")
+  } else {
+    navbar.classList.remove("fixed-top");
+  }
+}
 
