890117818e134c793a06c26125ca89e0a0bddee1
Adao Lima <adaoolima@gmail.com>
2019-04-17 23:07:17 -0300

    Improved the mobile menu interation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -25,35 +25,58 @@ $(document).ready(()=>{
         });
     });
 
-    $('.c-search__append button').click(function(ev){
-        ev.preventDefault();
-        isExpanded = true;
-        $(this).closest('body').toggleClass('is-expanded');
-        $('#navbar .c-search__input').focus();
-        
+   //var isExpanded = false;
+
+    // $('.c-search__append').click(function(ev){
+    //     ev.preventDefault();
+    //     if (!isExpanded) {
+    //         $('.c-search__append').addClass('is-button');
+    //         $('body').toggleClass('is-expanded');
+    //         $('#navbar .c-search__input').focus();
+    //         $('.c-search__append').find('button').on('click', function(){
+    //             $('#navbar .c-search__input').submit();
+    //         });
+    //         isExpanded = true;
+    //     } else {
+    //         $('body').removeClass('is-expanded');
+    //         isExpanded = false;
+    //     }
+    // }); 
+    
+    $('.c-search--toggle').on('click', function(){
+        $(this).find('span').toggleClass('icon-search icon-close');
+        $('body').toggleClass('is-expanded');
+        //$('#navbar .c-search__input').focus();
     });
+
+    // $(window).on('resize', function(){
+    //     var w = window.innerWidth;
+    //     if (w < 992) {
+    //         $('body').removeClass('is-expanded');
+            
+    //     }
+    // })
+    
+
 });
 
 // HAMMERJS
-// var w = window.innerWidth;
+var w = window.innerWidth;
 
-// if (w <= 992) {
-//     console.log(w);
-//     var myElement = document.getElementById('site'),
-//     myMenu = $('#navbarCollapse');
-//     // create a simple instance
-//     // by default, it only adds horizontal recognizers
-//     var mc = new Hammer(myElement);
+if (w <= 992) {
+    // console.log(w);
+    var myElement = document.getElementById('navbarCollapse'),
+    myMenu = $('#navbarCollapse');
+    // create a simple instance
+    // by default, it only adds horizontal recognizers
+    var mc = new Hammer(myElement);
 
-//     // listen to events...
-//     mc.on("panleft panright tap press", function(ev) {
-//         if (ev.type == "panright") {
-//             //console.log("RIGHT");
-//             myMenu.addClass('active');
-//         } else if (ev.type == "panleft") {
-//             //console.log("LEFT");
-//             myMenu.removeClass('active');
-//         }
-//     });
+    // listen to events...
+    mc.on("panleft panright tap press", function(ev) {
+        if (ev.type == "panleft") {
+            //console.log("LEFT");
+            myMenu.removeClass('active');
+        }
+    });
 
-// } 
+} 
