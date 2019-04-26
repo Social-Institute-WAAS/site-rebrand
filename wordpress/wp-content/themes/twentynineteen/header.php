72ee63a80cfc02892ad728651614aae571f44927
Adão Lima <adaoolima@gmail.com>
2019-04-26 13:11:45 -0300

    Merged

--- wordpress/wp-content/themes/twentynineteen/header.php
+++ wordpress/wp-content/themes/twentynineteen/header.php
@@ -10,9 +10,7 @@
  * @subpackage Twenty_Nineteen
  * @since 1.0.0
  */
-?>
-
-<!doctype html>
+?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -39,9 +37,9 @@
 						$discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null;
 
 						$classes = 'entry-header';
-						if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) {
-							$classes = 'entry-header has-discussion';
-						}
+					if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) {
+						$classes = 'entry-header has-discussion';
+					}
 					?>
 					<div class="<?php echo $classes; ?>">
 						<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
@@ -49,7 +47,6 @@
 					<?php rewind_posts(); ?>
 				</div>
 			<?php endif; ?>
-			
 		</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
